e2c8fd598c4d147bc9c4bc5a7d908e9fa6c921e0
fesswood <fesswood@kemsu.ru>
2015-06-15 08:00:58 +0700

    delete old stars and planets

--- starsandplanets/src/main/java/info/goodline/starsandplanets/activity/ViewPagerCallback.java
+++
@@ -1,8 +0,0 @@
-package info.goodline.starsandplanets.activity;
-
-/**
- * Created by sergeyb on 11.06.15.
- */
-public interface ViewPagerCallback {
-    void setCurrentItem(int pos);
-}
--- starsandplanets/src/main/java/info/goodline/starsandplanets/activity/VisibilityChangeCallback.java
+++
@@ -1,8 +0,0 @@
-package info.goodline.starsandplanets.activity;
-
-/**
- * Created by sergeyb on 11.06.15.
- */
-public interface VisibilityChangeCallback {
-    public void changeViewVisibility(int visibility);
-}
